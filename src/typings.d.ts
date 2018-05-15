51ac7bbab57ed1483c7c0a1e980cc543f366dd94
Ted Robinson <tfqbtd@aol.com>
2018-05-15 08:02:21 -0500

    Initial commit

---
+++ src/typings.d.ts
@@ -0,0 +1,14 @@
+/* SystemJS module definition */
+/* declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+} */
+import { DataTypeAbstract, DefineAttributeColumnOptions } from "sequelize";
+
+type SequelizeAttribute = string | DataTypeAbstract | DefineAttributeColumnOptions;
+
+declare global {
+  type SequelizeAttributes<T extends { [key: string]: any }> = {
+    [P in keyof T]: SequelizeAttribute
+  };
+}
